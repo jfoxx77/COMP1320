a5f61dd1422c9f6ccd37e4b5836a6c2214f0c932
jfoxx77 <jsam07@hotmail.com>
2020-09-16 15:15:39 -0700

    Adding Fahrenhiet to Celsius conversion

--- Lecture1/temperatureConversion.js
+++ Lecture1/temperatureConversion.js
@@ -1,5 +1,11 @@
+function convertFahrenheitToCelsius(tempInFahrenheit)
+{
+    return (((tempInFahrenheit - 32) *  5/9));
+}
+
 function convertCelsiusToFahrenheit(tempInCelsius)
 {
     return (((tempInCelsius * 9/5) + 32));
 }
+
 
