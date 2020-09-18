f79d1a4372aa7bc580b2e5df93ebe73fde40142e
jfoxx77 <jsam07@hotmail.com>
2020-09-18 13:30:57 -0700

    Updating functions to airbnb style guide and printing number of movies

--- Lecture1/favoriteMovies.js
+++ Lecture1/favoriteMovies.js
@@ -1,19 +1,18 @@
 
 let favoriteMovies = [];
 
-function addFavoriteMovie(movieName)
-{
-    let isStringValid = !movieName.includes("The");
+function addFavoriteMovie(movieName) {
+    let isStringValid = !movieName.includes('The');
     
-    if (isStringValid)
+    if (isStringValid) {
         favoriteMovies.push(movieName);
+    }
 }
 
 
-function printFavoriteMovies()
-{
-    console.log("Favorite Movies: \n")
-
+function printFavoriteMovies() {
+    console.log('Favorite Movies: ..');
+    console.log('The total number of movies is: ', favoriteMovies.length);
     for (let i = 0; i < favoriteMovies.length; i++) {
         console.log(favoriteMovies[i]);
     }
@@ -21,6 +20,9 @@ function printFavoriteMovies()
 
 // addFavoriteMovie("Spidernman");
 // addFavoriteMovie("Harry Potter");
+// addFavoriteMovie("Avatar");
+// addFavoriteMovie("Ex Machina");
+// addFavoriteMovie("Hobbit");
 // addFavoriteMovie("The Man In the High Castle");
 
 // printFavoriteMovies();
--- Lecture1/temperatureConversion.js
+++ Lecture1/temperatureConversion.js
@@ -1,10 +1,8 @@
-function convertFahrenheitToCelsius(tempInFahrenheit)
-{
+function convertFahrenheitToCelsius(tempInFahrenheit) {
     return (((tempInFahrenheit - 32) *  5/9));
 }
 
-function convertCelsiusToFahrenheit(tempInCelsius)
-{
+function convertCelsiusToFahrenheit(tempInCelsius) {
     return (((tempInCelsius * 9/5) + 32));
 }
 
